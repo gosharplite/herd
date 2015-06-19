3cfbeba077f1a65d13063161a3d82971d9eb7a5e
gosharplite <gosharplite@gmail.com>
2015-06-20 05:44:11 +0800

    Remove slog.

--- log/log.go
+++ log/log.go
@@ -4,18 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"log/syslog"
 )
-
-var slog *syslog.Writer
-
-func init() {
-	var err error
-	slog, err = syslog.New(syslog.LOG_INFO, "[herd]")
-	if err != nil {
-		log.Printf("syslog.New err: %s", err)
-	}
-}
 
 func Info(f string, v ...interface{}) string {
 	s := fmt.Sprintf(f, v...)
@@ -25,7 +14,6 @@ func Info(f string, v ...interface{}) string {
 
 func Err(f string, v ...interface{}) string {
 	s := fmt.Sprintf(f, v...)
-	slog.Err(s)
 	log.Printf(s)
 	return s
 }
@@ -35,7 +23,6 @@ func Marshal(v interface{}) string {
 	j, err := json.MarshalIndent(v, "", "    ")
 	if err != nil {
 		e := "Err: json.MarshalIndent()"
-		slog.Err(e)
 		log.Printf(e)
 		return ""
 	}
